c035f71dd605fc1155962fa59592d9bf60362c1e
Elioth <dharsuthdra@gmail.com>
2025-05-25 13:18:04 +0700

    Fixing Bugs

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -335,11 +335,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgres://tripitropi:tripitropi123@localhost:5432/kawantani"
+        "value": null
       }
     }
   },
