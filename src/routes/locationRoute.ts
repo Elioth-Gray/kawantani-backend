cac7f584e9937be8fa889486a3e54eb0c3b573d7
Elioth <dharsuthdra@gmail.com>
2025-06-15 14:39:52 +0700

    Fixing Location

--- src/routes/locationRoute.ts
+++ src/routes/locationRoute.ts
@@ -14,31 +14,31 @@ const locationRoute = express.Router();
 locationRoute.get(
   '/provinces',
   authMiddleware,
-  roleMiddleware(['facilitator', 'admin']),
+  roleMiddleware(['facilitator', 'admin', 'user']),
   getProvince,
 );
 locationRoute.get(
   '/provinces/:id',
   authMiddleware,
-  roleMiddleware(['facilitator', 'admin']),
+  roleMiddleware(['facilitator', 'admin', 'user']),
   getProvinceId,
 );
 locationRoute.get(
   '/regencies',
   authMiddleware,
-  roleMiddleware(['facilitator', 'admin']),
+  roleMiddleware(['facilitator', 'admin', 'user']),
   getRegency,
 );
 locationRoute.get(
   '/regencies/:id',
   authMiddleware,
-  roleMiddleware(['facilitator', 'admin']),
+  roleMiddleware(['facilitator', 'admin', 'user']),
   getRegencyId,
 );
 locationRoute.get(
   '/provinces/:id/regencies',
   authMiddleware,
-  roleMiddleware(['facilitator', 'admin']),
+  roleMiddleware(['facilitator', 'admin', 'user']),
   getRegencyProvinceId,
 );
 
