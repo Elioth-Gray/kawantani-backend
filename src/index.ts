7929766d6a01b31c001952ec68b27f1c0e7759dc
Elioth <dharsuthdra@gmail.com>
2025-06-22 19:17:09 +0700

    Deployment

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
 import userRoute from './routes/usersRoute';
 import authRoute from './routes/authRoute';
-import cors from 'cors';
 import facilitatorRoute from './routes/facilitatorRoute';
 import locationRoute from './routes/locationRoute';
-import path from 'path';
 import articlesRouter from './routes/articlesRoute';
 import workshopsRouter from './routes/workshopsRoute';
 import articleCategoriesRoute from './routes/articleCategoryRoute';
@@ -22,11 +22,15 @@ const app = express();
 
 const PORT = process.env.PORT || 2000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use(cors());
-
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+
+app.get('/', (req, res) => {
+  res.send('🚀 API is running!');
+});
+
 app.use('/api', authRoute);
 app.use('/api', userRoute);
 app.use('/api', facilitatorRoute);
@@ -40,6 +44,7 @@ app.use('/api', plantRoute);
 app.use('/api', usePlantsRoute);
 app.use('/api', adminRoute);
 
+// Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
